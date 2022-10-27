7823f47c44b681778aae6749c66db09d35f2a577
CJ <kubachod@gmail.com>
2022-10-27 19:11:53 +0200

    Initial Commit

---
+++ ApiFetcher/ApiFetcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApiFetcherApp.swift
+//  ApiFetcher
+//
+//  Created by Jakub Chodara on 27/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct ApiFetcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
